310adb0b1adf01fc18da1af1a8068a1898a889c9
Abhishekh Ugle <CU825Abhishekh@etechtexas.com>
2026-09-09 05:38:00 -0700

    Typescript folder

---
+++ Typescript/Primitive Types/Primitive.js
@@ -0,0 +1,10 @@
+"use strict";
+var num = 10;
+var num = 10.10;
+console.log(num);
+// Symbol 
+const sym1 = Symbol();
+const sym2 = Symbol("description");
+const sym3 = Symbol("description");
+console.log(sym1 === sym2);
+console.log(sym2 === sym3);
---
+++ Typescript/Primitive Types/Primitive.ts
@@ -0,0 +1,13 @@
+
+
+var num:number =10;
+var num:number=10.10;
+console.log(num);
+
+// Symbol 
+const sym1 = Symbol();
+const sym2 = Symbol("description");
+const sym3 = Symbol("description");
+
+console.log(sym1 === sym2);
+console.log(sym2 === sym3);
