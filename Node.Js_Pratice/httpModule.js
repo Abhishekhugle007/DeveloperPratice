da303a208dd83ad00bad3c816bc64fef0c702d80
Abhishekh Ugle <CU825Abhishekh@etechtexas.com>
2026-09-26 02:45:33 -0700

    node js

--- Node.Js_Pratice/httpModule.js
+++ Node.Js_Pratice/httpModule.js
@@ -1,1 +1,10 @@
 const http = require('http');
+const server  = http.createServer((req, res)=>{
+    res.write('Hello,World');
+    res.end();
+});
+server.listen(3000,()=>{
+    console.log("Server is listening on port 3000");
+});
+
+
